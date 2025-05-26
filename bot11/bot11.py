0fda72027a39851f018b65a0c4b9754d64722b3e
the3kproduction <the3kproduction@gmail.com>
2025-05-26 11:32:17 -0700

    Create bot11.py

---
+++ bot11/bot11.py
@@ -0,0 +1,32 @@
+import discord
+import os
+from discord.ext import tasks
+from keep_alive import keep_alive
+
+TOKEN = os.getenv("DISCORD_TOKEN_11")
+CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID_11"))
+
+intents = discord.Intents.default()
+intents.voice_states = True
+
+class VoiceClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected')
+        channel = self.get_channel(CHANNEL_ID)
+        if channel:
+            vc = await channel.connect()
+            vc.play(discord.FFmpegPCMAudio("silent.mp3"), after=lambda e: print('done', e))
+            loop.start(vc)
+        else:
+            print("Channel not found")
+
+@tasks.loop(seconds=60)
+async def loop(vc):
+    if not vc.is_playing():
+        vc.play(discord.FFmpegPCMAudio("silent.mp3"))
+
+client = VoiceClient(intents=intents)
+
+if __name__ == "__main__":
+    keep_alive()
+    client.run(TOKEN)

079b869fbd995711095e0af297fd866d35b75d8b
the3kproduction <the3kproduction@gmail.com>
2025-05-25 21:08:04 -0700

    Add files via upload

---
+++ bot3.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from discord.ext import tasks
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID"))
+
+class VoiceClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} has connected')
+        channel = self.get_channel(CHANNEL_ID)
+        if channel:
+            vc = await channel.connect()
+            vc.play(discord.FFmpegPCMAudio("silent.mp3"), after=lambda e: print('done', e))
+            loop.start(vc)
+        else:
+            print("Channel not found")
+
+@tasks.loop(seconds=60)
+async def loop(vc):
+    if not vc.is_playing():
+        vc.play(discord.FFmpegPCMAudio("silent.mp3"))
+
+client = VoiceClient()
+client.run(TOKEN)

e2d99f88bfda680bbdfff1187d96c8e9d8309e6d
the3kproduction <the3kproduction@gmail.com>
2025-05-25 23:16:52 -0700

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -5,16 +5,6 @@ from discord.ext import tasks
 TOKEN = os.getenv("DISCORD_TOKEN_2")
 CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID_2"))
 
-class VoiceClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected')
-        import discord
-import os
-from discord.ext import tasks
-
-TOKEN = os.getenv("DISCORD_TOKEN_2")
-CHANNEL_ID = int(os.getenv("VOICE_CHANNEL_ID_2"))
-
 # Set up the required intents
 intents = discord.Intents.default()
 intents.voice_states = True  # Enable voice state updates (required for joining channels)
@@ -37,20 +27,4 @@ async def loop(vc):
 
 # Pass the intents when initializing the client
 client = VoiceClient(intents=intents)
-client.run(TOKEN)
-
-        channel = self.get_channel(CHANNEL_ID)
-        if channel:
-            vc = await channel.connect()
-            vc.play(discord.FFmpegPCMAudio("silent.mp3"), after=lambda e: print('done', e))
-            loop.start(vc)
-        else:
-            print("Channel not found")
-
-@tasks.loop(seconds=60)
-async def loop(vc):
-    if not vc.is_playing():
-        vc.play(discord.FFmpegPCMAudio("silent.mp3"))
-
-client = VoiceClient()
 client.run(TOKEN)
